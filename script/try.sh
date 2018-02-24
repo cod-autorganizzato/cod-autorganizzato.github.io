eb0e8a17cd1aa86a605fe93113065e0bce9f37d6
cod-autorganizzato <cod.autorganizzato@gmail.com>
2018-02-24 20:42:20 +0100

    rm

--- script/try.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  
-python ./create_json.py
-git add ../JSON/data.json
-git commit -m "aggiornati dati" -m "check check one two one two";
-git push https://cod-autorganizzato:Giavaskript23@github.com/cod-autorganizzato/cod-autorganizzato.github.io
-
