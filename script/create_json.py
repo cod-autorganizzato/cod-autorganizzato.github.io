df289ccafe0861d0043c9c51981c0d5e41e908d5
cod-autorganizzato <cod.autorganizzato@gmail.com>
2018-03-10 20:24:58 +0100

    fix coder

--- script/create_json.py
+++ script/create_json.py
@@ -38,7 +38,7 @@ class info(object):
 
 if __name__ == '__main__':
     site = urllib.urlopen(home)
-    soup = bs(site, 'html.parser')
+    soup = bs(site, 'html5lib')
     soup = str(soup).decode('utf-8','ignore').split('AGGRESSIONI FASCISTE')[-1]
     data = [info(x).info for x in soup.split('</h3><h3>')]
     with open('../JSON/data.json', 'w') as outfile:
